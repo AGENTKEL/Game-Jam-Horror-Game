5865ee0967665550691797cca2264d0e98e3c27a
RJ <108998789+Rjwest07@users.noreply.github.com>
2023-04-20 01:14:54 +1000

    saved my stuff as prefabs for merge
    hopefully my tags and layers get saved too

--- Game Jam Horror Game/Assets/Scripts/InteractionManager.cs
+++ Game Jam Horror Game/Assets/Scripts/InteractionManager.cs
@@ -4,32 +4,78 @@ using UnityEngine;
 
 public class InteractionManager : MonoBehaviour
 {
-    RaycastHit hit;
+    
 
     [SerializeField] private LayerMask collisionMask;
     [SerializeField] private float rayDistance;
+    RaycastHit hit;
     private Ray ray;
 
     public GameObject handUI;
     public Transform PCAM;
 
+    static public bool KeypadUI;
+    public GameObject KeypadDisplay;
+
     void Awake() 
     {
-        ray = new Ray (PCAM.position, PCAM.forward);
+        
     }
 
     void Update()
     {
-        Debug.DrawRay(PCAM.position, PCAM.forward * rayDistance, Color.red);
-        if (Physics.Raycast (ray, out hit, rayDistance, collisionMask))
+        switch (KeypadUI)
         {
-            handUI.SetActive(true);
-            if (Input.GetKeyDown(KeyCode.E))
+        case false:
+            //make sure player can move and update where we raycast
+            ray = new Ray (PCAM.position, PCAM.forward);
+            KeypadDisplay.SetActive(false);
+            disablePlayer();
+
+            if (Physics.Raycast (ray, out hit, rayDistance, collisionMask))
             {
-                print("got it");
+                handUI.SetActive(true);
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    WhichThingWasInteractedWith();
+                }
             }
+            else
+                handUI.SetActive(false);
+            break;
+        case true:
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                KeypadUI = false;
+                KeypadDisplay.SetActive(false);
+                disablePlayer();
+            }
+            break;
         }
-        else
-            handUI.SetActive(false);
+    }
+
+    private void WhichThingWasInteractedWith()
+    {
+        if (hit.collider.tag == "Keypad" && KeypadUI == false)
+            {
+                KeypadUI = true;
+                KeypadDisplay.SetActive(true);
+                disablePlayer();
+            }
+    }
+
+    private void disablePlayer()
+    {
+        switch (KeypadUI)
+        {
+        case true:
+            PlayerController.movementEnabled = false;
+            Cursor.lockState = CursorLockMode.None;
+            break;
+        case false:
+            PlayerController.movementEnabled = true;
+            Cursor.lockState = CursorLockMode.Locked;
+            break;
+        }
     }
 }
